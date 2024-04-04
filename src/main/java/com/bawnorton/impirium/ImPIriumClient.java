89f763b8ef9d199bfa5c1baa32ee5411b9e73433
Thosea <104597976+ItsThosea@users.noreply.github.com>
2024-04-03 21:52:16 -0400

    Add client command suffix when playing multiplayer and add warning

--- src/main/java/com/bawnorton/impirium/ImPIriumClient.java
+++ src/main/java/com/bawnorton/impirium/ImPIriumClient.java
@@ -4,22 +4,51 @@ import com.mojang.brigadier.arguments.DoubleArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 public class ImPIriumClient implements ClientModInitializer {
+	private boolean showedWarning;
+
 	@Override
 	public void onInitializeClient() {
+		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+			showedWarning = false;
+		});
+
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-			dispatcher.register(ClientCommandManager.literal("getpi")
+			MinecraftClient client = MinecraftClient.getInstance();
+			boolean singleplayer = client.isInSingleplayer();
+			String suffix = singleplayer ? "" : "client";
+
+			if(!singleplayer && ImPIrium.NEW_PI != 3.141592653589793) {
+				client.inGameHud.getChatHud().addMessage(Text.literal("Warning: your PI is modified on a public server, you may get banned by an anticheat.")
+						.formatted(Formatting.RED, Formatting.BOLD));
+			}
+
+			dispatcher.register(ClientCommandManager.literal("getpi" + suffix)
 					.executes(context -> {
 						context.getSource().sendFeedback(Text.of("Pi is " + ImPIrium.NEW_PI));
 						return 1;
 					})
 			);
-			dispatcher.register(ClientCommandManager.literal("setpi")
+			dispatcher.register(ClientCommandManager.literal("setpi" + suffix)
 					.then(ClientCommandManager.argument("value", DoubleArgumentType.doubleArg())
 							.executes(context -> {
-								ImPIrium.NEW_PI = DoubleArgumentType.getDouble(context, "value");
+								double pi = DoubleArgumentType.getDouble(context, "value");
+
+								if(!showedWarning && !singleplayer && pi != 3.141592653589793) {
+									showedWarning = true;
+									context.getSource()
+											.sendFeedback(Text.literal("Warning: you may get banned by an anticheat." +
+															"\nAre you sure? (run again to confirm)")
+													.formatted(Formatting.RED, Formatting.BOLD));
+									return 1;
+								}
+
+								ImPIrium.NEW_PI = pi;
 								ImPIrium.updateValues();
 								context.getSource().sendFeedback(Text.of("Pi set to " + ImPIrium.NEW_PI));
 								return 1;
